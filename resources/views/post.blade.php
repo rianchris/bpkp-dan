373fe7514a06261687dddee73d788926f55be2b8
Rian Chris <chrissesario.rian@gmail.com>
2023-02-23 21:33:41 +0700

    belajar

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,15 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h3>{{ $post->title }}</h3>
-
-                <p> By <a href="/author/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}
+                <div class="share-button">
+                    <a href="whatsapp://send?text=bpkp-dan.test/{{ $post->slug }}"><i class="bi bi-whatsapp"
+                            style="font-size: 1.5rem; color: dodgerblue"></i></a>
+                    <i class="bi bi-facebook" style="font-size: 1.5rem; color: dodgerblue"></i>
+                    <i class="bi bi-envelope" style="font-size: 1.5rem; color: dodgerblue"></i>
+                    <i class="bi bi-telegram" style="font-size: 1.5rem; color: dodgerblue"></i>
+                </div>
+                <p> By <a href="/author/{{ $post->author->username }}"
+                        class="text-decoration-none">{{ $post->author->name }}
                     </a> in <a href="/categories/{{ $post->category->slug }}"
                         class="text-decoration-none">{{ $post->category->name }}</a></p>
                 <img src="https://source.unsplash.com/800x400/?{{ $post->category->label }}"
